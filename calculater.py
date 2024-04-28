826d414d1d3af78ae8a722fd7ccea02c975cd0bb
javedakhtar2003 <122269037+javedakhtar2003@users.noreply.github.com>
2024-04-28 16:37:46 +0530

    chnages and delete files

--- calculater.py
+++ calculater.py
@@ -6,16 +6,16 @@ num2=int(input("enter the second number:"))
 operator=input("what do you want to do(+,-,*,/):")
 if operator=="+":
     sum=int(num1+num2)
-    print("addition of two numbers=",sum)
+    print("addision of",num1, " and ",num2, " = ",sum)
 elif operator=="-":
     sub=int(num1-num2)
-    print("subtraction of two numbers=",sub)
+    print("subtraction of",num1, " and ",num2, " = ",sub)
 elif operator=="*":
     mul=int(num1*num2)
-    print("multiplication of two numbers=",mul)
+    print("multiplication of",num1, " and ",num2, " = ",mul)
 elif operator=="/":
     div=int(num1/num2)
-    print("division of two number=",div)
+    print("division of",num1, " and ",num2, " = ",div)
 else:
     print("sorry operator is not exist in list")
    
--- calendar1.py
+++ calendar1.py
@@ -2,4 +2,3 @@ import calendar as c
 year=int(input("enter the year ="))
 month=int(input("enter the month ="))
 print(c.month(year,month))
-
